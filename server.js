a7a255f15387d61c7ad462c2453e72f1308af619
adityapandey <89adityapandey@gmail.com>
2026-09-16 23:04:29 +0530

    added dynamic route with route and query parameters

--- server.js
+++ server.js
@@ -4,40 +4,19 @@ const app = express();
 
 app.use(express.json());
 
-// Custom middleware
-const setUser = (req, res, next) => {
-    req.user = "Guest";
-    next();
-};
+// Dynamic GET route with route parameter and query parameter
+app.get("/welcome/:username", (req, res) => {
+    const username = req.params.username;
+    const role = req.query.role;
 
-app.get("/", (req, res) => {
-    res.send("hello from express server ");
-});
-
-// Route where setUser middleware is applied
-app.get("/welcome", setUser, (req, res) => {
-    res.send(`<h1>Welcome, ${req.user}!</h1>`);
-});
-
-// Orders endpoints
-app.get("/orders", (req, res) => {
-    res.send("Here is the list of all orders.");
-});
-
-app.post("/orders", (req, res) => {
-    res.send("A new order has been created.");
-});
-
-// Users endpoints
-app.get("/users", (req, res) => {
-    res.send("Here is the list of all users.");
-});
-
-app.post("/users", (req, res) => {
-    res.send("A new user has been added.");
+    if (role) {
+        res.send(`Welcome ${username}, your role is ${role}`);
+    } else {
+        res.send(`Welcome ${username}`);
+    }
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-});
+});
