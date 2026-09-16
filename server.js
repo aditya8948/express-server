b999c6d7d853f559ffb305f628ab6ea337496e0e
adityapandey <89adityapandey@gmail.com>
2026-09-16 12:06:32 +0530

    added custom middleware

--- server.js
+++ server.js
@@ -4,8 +4,19 @@ const app = express();
 
 app.use(express.json());
 
+// Custom middleware function to add req.user property
+const setUser = (req, res, next) => {
+    req.user = "Guest";
+    next();
+};
+
 app.get("/", (req, res) => {
     res.send("hello from express server ");
+});
+
+// Route where setUser middleware is applied
+app.get("/welcome", setUser, (req, res) => {
+    res.send(`<h1>Welcome, ${req.user}!</h1>`);
 });
 
 
